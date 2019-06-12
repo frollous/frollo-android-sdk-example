713304ce6c8368eeac14ec0fc15a771aba8fd397
Deepak Marasandra <deepak@frollo.us>
2019-06-12 14:26:28 +1000

    [SDk-199] Integrate SDK v1.4.0

---
+++ FrolloSDKSample/src/main/kotlin/us/frollo/frollosdksample/view/aggregation/adapters/TagsSearchAdapter.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 Frollo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package us.frollo.frollosdksample.view.aggregation.adapters
+
+import android.view.View
+import kotlinx.android.synthetic.main.template_simple_item1.view.text_title
+import us.frollo.frollosdk.model.coredata.aggregation.tags.TransactionTag
+import us.frollo.frollosdksample.R
+import us.frollo.frollosdksample.base.BaseRecyclerAdapter
+import us.frollo.frollosdksample.base.BaseViewHolder
+
+class TagsSearchAdapter : BaseRecyclerAdapter<TransactionTag, TagsSearchAdapter.TagViewHolder>(TransactionTag::class.java) {
+
+    override fun getViewHolderLayout(viewType: Int) =
+            R.layout.template_simple_item1
+
+    override fun getViewHolder(view: View, viewType: Int) =
+            TagViewHolder(view)
+
+    inner class TagViewHolder(itemView: View) : BaseViewHolder<TransactionTag>(itemView) {
+
+        override fun bind(model: TransactionTag) {
+            itemView.text_title.text = model.name
+        }
+
+        override fun recycle() {
+            itemView.text_title.text = null
+        }
+    }
+}
