5c763fb820342f9f6c5b954d4213f8d81ce5444a
Deepak Marasandra <deepak@frollo.us>
2019-07-31 12:16:26 +1000

    User info in profile

--- FrolloSDKSample/src/main/kotlin/us/frollo/frollosdksample/view/profile/ProfileActivity.kt
+++ FrolloSDKSample/src/main/kotlin/us/frollo/frollosdksample/view/profile/ProfileActivity.kt
@@ -21,9 +21,12 @@ import kotlinx.android.synthetic.main.activity_profile.*
 import org.jetbrains.anko.alert
 import org.jetbrains.anko.startActivity
 import us.frollo.frollosdk.FrolloSDK
+import us.frollo.frollosdk.base.Resource
 import us.frollo.frollosdksample.R
 import us.frollo.frollosdksample.base.BaseStackActivity
 import us.frollo.frollosdksample.managers.SetupManager
+import us.frollo.frollosdksample.utils.displayError
+import us.frollo.frollosdksample.utils.observe
 import us.frollo.frollosdksample.utils.showThemed
 import us.frollo.frollosdksample.view.authentication.LoginActivity
 
@@ -33,6 +36,19 @@ class ProfileActivity : BaseStackActivity() {
         super.onCreate(savedInstanceState)
 
         btn_logout.setOnClickListener { logout() }
+        initLiveData()
+    }
+
+    private fun initLiveData() {
+        FrolloSDK.userManagement.fetchUser().observe(this) {
+            when (it?.status) {
+                Resource.Status.SUCCESS -> it.data?.let { user ->
+                    name.text = "${ user.firstName } ${ user.lastName }"
+                    email.text = user.email
+                }
+                Resource.Status.ERROR -> displayError(it.error?.localizedDescription, "Fetch User Failed")
+            }
+        }
     }
 
     private fun logout() {
